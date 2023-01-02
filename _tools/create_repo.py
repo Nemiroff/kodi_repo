f18452d5aa4c0c2d8ce5eb6faa54042a7596f931
Nemiroff <nemiroff@xaker.ru>
2023-01-02 12:02:09 +0500

    update to py3

--- _tools/create_repo.py
+++ _tools/create_repo.py
@@ -5,18 +5,18 @@ import stat
 import re
 import zipfile
 import shutil
-import urllib2
 import json
 import argparse
 from xml.dom import minidom
 from hashlib import md5
-from ConfigParser import SafeConfigParser
+import urllib.request as urllib2
+from configparser import ConfigParser
 
 
 class main:
     def __init__(self, py_ver):
         self.py_ver = py_ver
-        self.config = SafeConfigParser()
+        self.config = ConfigParser()
         self.config.read("config.ini")
         self.tools_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__))))
         self.output_path = "pages/repo/"
@@ -35,7 +35,7 @@ class main:
         print("Finished updating addons xml, md5 files and zipping addons")
 
     def _pre_run(self):
-        # create output  path if it does not exists
+        # create output  path if it does not exist
         if not os.path.exists(self.output_path):
             os.makedirs(self.output_path)
 
@@ -58,14 +58,15 @@ class main:
         if not os.path.exists(temp_path):
             os.makedirs(temp_path)
         req = urllib2.Request("https://api.github.com/repos/{0}/releases/{1}".format(clone_repo, release_version))
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.21 Safari/537.36')
+        req.add_header('User-Agent',
+                       'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.21 Safari/537.36')
         req.add_header('Authorization', 'token {0}'.format(os.environ.get('MY_TOKEN')))
         infostring = urllib2.urlopen(req).read()
         infostring = json.loads(infostring.decode('utf-8', 'replace'))
         print('Get {1} asserts for {0}'.format(name, release_version))
         for file in infostring['assets']:
-            self.download_binary(file['browser_download_url'], temp_path+name+'.zip')
-            zip = zipfile.ZipFile(temp_path+name+'.zip')
+            self.download_binary(file['browser_download_url'], temp_path + name + '.zip')
+            zip = zipfile.ZipFile(temp_path + name + '.zip')
             zip.extractall(self.path_ad)
             zip.close()
             git_folder_path = os.path.abspath(os.path.join(self.path_ad, name, ".git"))
@@ -79,7 +80,7 @@ class main:
 
     def _generate_repo_files(self):
         # addon list
-        addons = os.listdir( "." )
+        addons = os.listdir(".")
         addons.remove('.git')
         addons.remove('_tools')
         addons.remove('.gitignore')
@@ -89,21 +90,21 @@ class main:
         addons.remove('_py3')
         # final addons text
         addons_xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addons>\n"
-        # loop thru and add each addons addon.xml file
+        # loop through and add each addons addon.xml file
         for addon in addons:
             # create path
-            _path = os.path.join( addon, "addon.xml" )
-            #skip path if it has no addon.xml
-            if not os.path.isfile( _path ): continue
-            try:               
+            _path = os.path.join(addon, "addon.xml")
+            # skip path if it has no addon.xml
+            if not os.path.isfile(_path): continue
+            try:
                 # split lines for stripping
-                xml_lines = open( _path, "r" ).read().splitlines()
+                xml_lines = open(_path, "r").read().splitlines()
                 # new addon
                 addon_xml = ""
                 # loop thru cleaning each line
                 for line in xml_lines:
                     # skip encoding format line
-                    if line.find( "<?xml" ) >= 0: continue
+                    if line.find("<?xml") >= 0: continue
                     if self.py_ver == 3:
                         line = line.replace("%VERSION%", "~matrix").replace("%PY_VER%", "_matrix")
                     else:
@@ -111,12 +112,12 @@ class main:
                     if self.py_ver == 3 and line.find('"xbmc.python"') >= 0:
                         line = '        <import addon="xbmc.python" version="3.0.0"/>'
                     # add line
-                    addon_xml += unicode( line.rstrip() + "\n", "utf-8" )
+                    addon_xml += line.rstrip() + "\n"
                 # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
             except Exception as e:
                 # missing or poorly formatted addon.xml
-                print("Excluding %s for %s" % ( _path, e, ))
+                print("Excluding %s for %s" % (_path, e,))
         if self.py_ver == 2:
             addons_xml += self._add_repo_py3().decode("utf-8").strip()
         # clean and add closing tag
@@ -125,7 +126,7 @@ class main:
         filename = "addons"
         if self.py_ver == 3:
             filename += "_matrix"
-        self._save_file( addons_xml.encode( "utf-8" ), file=self.output_path + filename +".xml" )
+        self._save_file(addons_xml.encode("utf-8"), file=self.output_path + filename + ".xml")
 
     def _add_repo_py3(self):
         return """
@@ -154,10 +155,10 @@ class main:
     def _save_file(self, data, file):
         try:
             # write data to the file
-            open( file, "w" ).write( data )
+            open(file, "w").write(data)
         except Exception as e:
             # oops
-            print("An error occurred saving %s file!\n%s" % ( file, e, ))
+            print("An error occurred saving %s file!\n%s" % (file, e,))
 
     def _generate_md5_file(self):
         try:
@@ -165,30 +166,31 @@ class main:
             name = "addons"
             if self.py_ver == 3:
                 name += "_matrix"
-            m = md5.new( open(self.output_path + name + ".xml" ).read() ).hexdigest()
+            m = md5.new(open(self.output_path + name + ".xml").read()).hexdigest()
             # save file
-            self._save_file( m, file=self.output_path + name + ".xml.md5" )
+            self._save_file(m, file=self.output_path + name + ".xml.md5")
         except Exception as e:
             # oops
-            print("An error occurred creating addons.xml.md5 file!\n%s" % ( e, ))
+            print("An error occurred creating addons.xml.md5 file!\n%s" % (e,))
 
     def _generate_zip_files(self):
-        addons = os.listdir( "." )
+        addons = os.listdir(".")
         addons.remove('.git')
         addons.remove('_tools')
         addons.remove('.gitignore')
         addons.remove('.travis.yml')
         addons.remove('pages')
         addons.remove('_py3')
-        # loop thru and add each addons addon.xml file
+        # loop through and add each addons addon.xml file
         for addon in addons:
             # create path
             _path = os.path.join(addon, "addon.xml")
-            #skip path if it has no addon.xml
+            # skip path if it has no addon.xml
             if not os.path.isfile(_path): continue
             try:
                 # skip any file or .git folder
-                if (not os.path.isdir(addon) or addon == ".git" or addon == self.output_path or addon == self.tools_path): continue
+                if (not os.path.isdir(
+                    addon) or addon == ".git" or addon == self.output_path or addon == self.tools_path): continue
                 # create path
                 _path = os.path.join(addon, "addon.xml")
                 # split lines for stripping
@@ -224,8 +226,10 @@ class main:
             shutil.move(filename, self.output_path + addonid + os.path.sep + filename)
             shutil.copy(path + os.path.sep + 'icon.png', self.output_path + addonid + os.path.sep + 'icon.png')
             shutil.copy(path + os.path.sep + 'fanart.jpg', self.output_path + addonid + os.path.sep + 'fanart.jpg')
-            shutil.copy(path + os.path.sep + 'changelog.txt', self.output_path + addonid + os.path.sep + 'changelog.txt')
-            shutil.copy(path + os.path.sep + 'changelog.txt', self.output_path + addonid + os.path.sep + 'changelog-' + version + '.txt')
+            shutil.copy(path + os.path.sep + 'changelog.txt',
+                        self.output_path + addonid + os.path.sep + 'changelog.txt')
+            shutil.copy(path + os.path.sep + 'changelog.txt',
+                        self.output_path + addonid + os.path.sep + 'changelog-' + version + '.txt')
         except Exception as e:
             print(e)
 
@@ -236,7 +240,7 @@ class main:
     def fix_addon_ver(self, path, version):
         addon_xml = ""
         try:
-            xml_lines = open( path, "r" ).read().splitlines()
+            xml_lines = open(path, "r").read().splitlines()
             for line in xml_lines:
                 if self.py_ver == 3:
                     line = line.replace("%VERSION%", "~matrix").replace("%PY_VER%", "_matrix")
@@ -244,14 +248,15 @@ class main:
                     line = line.replace("%VERSION%", "").replace("%PY_VER%", "")
                 if line.find('"xbmc.python"') >= 0 and self.py_ver == 3:
                     line = '        <import addon="xbmc.python" version="3.0.0"/>'
-                addon_xml += unicode( line.rstrip() + "\n", "utf-8" )
+                addon_xml += line.rstrip() + "\n"
         except Exception as e:
             addon_xml = ""
-            print("Excluding fix %s for %s" % ( path, e, ))
+            print("Excluding fix %s for %s" % (path, e,))
         return addon_xml.encode('utf-8')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('py_ver', metavar='int', type=int, choices=[2,3], help="Py version addon")
+    parser.add_argument('py_ver', metavar='int', type=int, choices=[2, 3], help="Py version addon")
     args = parser.parse_args()
     main(args.py_ver)
